dcd7e53c0f57eaf4c73a953b0eb0814094813c73
Alexander Martynsh <amartynsh@yandex.ru>
2024-10-18 11:30:15 +0300

    Правка замечаний PR

--- main-service/src/main/java/ru/practicum/ewm/categories/service/CategoriesServiceImpl.java
+++ main-service/src/main/java/ru/practicum/ewm/categories/service/CategoriesServiceImpl.java
@@ -3,12 +3,14 @@ package ru.practicum.ewm.categories.service;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import ru.practicum.ewm.categories.dto.CategoryDto;
 import ru.practicum.ewm.categories.mapper.CategoryMapper;
 import ru.practicum.ewm.categories.model.Category;
 import ru.practicum.ewm.categories.repository.CategoriesRepository;
 import ru.practicum.ewm.core.error.exception.NotFoundException;
 
+@Transactional(readOnly = true)
 @Service
 @Slf4j
 @RequiredArgsConstructor
@@ -16,12 +18,14 @@ public class CategoriesServiceImpl implements CategoriesService {
     private final CategoriesRepository categoriesRepository;
     private final CategoryMapper categoryMapper;
 
+    @Transactional
     @Override
     public Category addCategory(CategoryDto categoryDto) {
         Category category = categoryMapper.toEntity(categoryDto);
         return categoriesRepository.save(category);
     }
 
+    @Transactional
     @Override
     public Category updateCategory(long id, CategoryDto categoryDto) {
         log.info("start updateCategory");
@@ -33,6 +37,7 @@ public class CategoriesServiceImpl implements CategoriesService {
         return categoriesRepository.save(category);
     }
 
+    @Transactional
     @Override
     public void deleteCategory(Long id) {
         categoriesRepository.deleteById(id);
